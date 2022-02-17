51dcda258c21ca186fa304057fd7f4847c71d8c1
Tennison Capra <tennison_capra@hotmail.com>
2022-02-17 19:44:46 -0300

    very bad solution

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,55 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+
+        Scanner scan = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = scan.nextInt();
+        System.out.print("Check-in date (dd/yyyy): ");
+        Date checkIn = sdf.parse(scan.next());
+        System.out.print("Check-out date (dd/yyyy): ");
+        Date checkOut = sdf.parse(scan.next());
+
+        if(!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        }else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println("\nEnter data to update the reservation: ");
+            System.out.print("Check-in date (dd/yyyy): ");
+            checkIn = sdf.parse(scan.next());
+            System.out.print("Check-out date (dd/yyyy): ");
+            checkOut = sdf.parse(scan.next());
+
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation: Reservation dates must be future dates");
+            } else if (!checkOut.after(checkIn)){
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            } else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+
+
+        }
+
+
+
+
+        scan.close();
+    }
+
+}
